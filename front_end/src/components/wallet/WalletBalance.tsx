d5690f2ac419f2e6a12496f491c5dba62944915b
sonercand <s.ism.candan@gmail.com>
2022-04-08 10:24:44 +0000

    fix: ts to tsz

--- front_end/src/components/wallet/WalletBalance.tsx
+++ front_end/src/components/wallet/WalletBalance.tsx
@@ -1,17 +1,10 @@
 import { Token } from "../Main"
 import { useEthers, useTokenBalance } from "@usedapp/core"
-import { Box } from "@material-ui/core"
-interface WalletBalanceProbs {
+export interface WalletBalanceProbs {
     token: Token
 }
 export const WalletBalance = ({ token }: WalletBalanceProbs) => {
     const { image, address, name } = token
     const { account } = useEthers()
-    const tokenBalance = useTokenBalance(address, account)
-    console.log(tokenBalance?.toString())
-    return (
-        <div>a < /div>
-    )
-
-
+    return (<div>{account}</div>)
 }
